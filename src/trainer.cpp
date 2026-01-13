884a63fd4de9fba6fc6b67fbf8e50e7573cdede3
jps114-ua <jps114@alu.ua.es>
2026-01-13 16:06:07 +0100

    Implementando clase Trainer

--- src/trainer.cpp
+++ src/trainer.cpp
@@ -2,3 +2,60 @@
 #include "common.hpp"
 #include <iostream>
 
+std::vector<std::vector<double>> Trainer::computeDeltas(NeuralNetwork& nn, const std::vector<double>& target, ActivationType activation){
+	int num_layers = nn.layers.size();
+	std::vector<std::vector<double>> deltas(num_layers); 
+
+	int L = num_layers -1; 
+	int num_neurons_L = nn.layers[L].neurons.size(); 
+	deltas[L].resize(num_neurons_L);
+
+	for (int j=0; j < num_neurons; ++j){
+		double x_j_L = nn.layers[L].last_outputs[j]; //¿Puede acceder a esta variable?
+
+		//Derivada del error cuadrático 
+		double error_derivative = 2.0 * (x_j_L - target[j]);
+
+		double theta_prime = Activations::applyDerivative(x_j_L, activation);
+
+		deltas[L][j] = theta_prime * error_derivative;
+	}
+
+	for (int l = L -1; l >= 0; --l){
+		int num_neurons_l = nn.layers[l].neurons.size();
+		deltas[l].resize(num_neurons_l);
+
+		for (int i=0; i < num_neurons_l; ++i){
+			double sum_deltas_next = 0.0;
+
+			for (int j=0; j < nn.layers[l+1].neurons.size(); ++j){
+				sum_deltas_next += nn.layers[l+1].neurons[j].weights[i] * deltas[l+1][j];
+			}
+
+			double x_i_l = nn.layers[l].last_outputs[i];
+			double theta_prime = Activations::applyDerivative(x_i_l, activation);
+
+			deltas[l][i] = theta_prime * sum_deltas_next;
+		}
+	}
+
+	return deltas; 
+}
+
+
+void Trainer::trainBackprop(NeuralNetwork& nn, const std::vector<std::vector<double>>& data,
+							const std::vector<std::vector<double>>& targets, int epochs, ActivationType activation){
+
+	for (int e=0; e < epochs; ++e){
+		double total_error = 0.0;
+
+		for (size_t i=0; i < data_size(); ++i){
+			nn.predict(data[i], activation);
+
+			std::vector<std::vector<double>> deltas = computeDeltas(nn, targets[i], activation);
+
+			applyGradients(nn, deltas, this->learning_rate); 
+		}
+	}
+}
+
--- src/trainer.hpp
+++ src/trainer.hpp
@@ -1,6 +1,27 @@
 #ifndef TRAINER_HPP
 #define TRAINER_HPP
 
+#include "NeuralNetwork.hpp"
+#include <vector>
+
 //TODO: trainer will probably only be function helpers to loop-train neural networks
+
+class Trainer{
+private:
+	double learning_rate;
+
+	std::vector<std::vector<double>> computeDeltas(NeuralNetwork& nn, const std::vector<double>& target, ActivationType activation);
+
+	void applyGradients(NeuralNetwork& nn, const std::vector<std::vector<double>>& deltas, double eta);
+
+public:
+	Trainer(double lr = 0.1) : learning_rate(lr){}
+
+	void trainPLA(Perceptron& p, const std::vector<std::vector<double>>& data, const std::vector<int>& labels);
+
+	void trainBackprop(NeuralNetwork& nn, const std::vector<std::vector<double>>& data, const std::vector<std::vector<double>>& targets, 
+						int epochs, ActivationType activation);
+
+};
 
 #endif
