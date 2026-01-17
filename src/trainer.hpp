6e4915e445eee5e4d7be5675e6f4a96bd27eb185
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-01-17 03:31:27 +0100

    Finshed refactoring Trainer class, but lasts to compile and debug

--- src/trainer.hpp
+++ src/trainer.hpp
@@ -6,34 +6,35 @@
 
 using std::vector;
 
+
 class Trainer{
 private:
-	double learning_rate;
+    double learning_rate;
 
-	vector<vector<double>> computeDeltas(NeuralNetwork& nn, const vector<double>& target) const;
-	void applyGradients(NeuralNetwork& nn, const vector<vector<double>>& deltas, double eta) const;
+    vector<vector<double>> computeDeltas(NeuralNetwork& nn, int target_class) const;  // Changed to int
+    void applyGradients(NeuralNetwork& nn, const vector<vector<double>>& deltas, double eta) const;
 
 public:
-	Trainer(double lr = 0.1) : learning_rate(lr){}
+    Trainer(double lr = 0.1) : learning_rate(lr){}
 
-	double train(
-      NeuralNetwork& nn,
-      const vector<vector<double>>& data,
-      const vector<int>& targets,
-      const int epochs) const;
+    double train(
+        NeuralNetwork& nn,
+        const vector<vector<double>>& data,
+        const vector<int>& targets,
+        const int epochs) const;
 
-  double train(
-      Perceptron& perceptron,
-      const vector<vector<double>>& training_inputs,
-      const vector<double>& training_outputs,
-      double learning_rate = 0.1,
-      int max_epochs = 1000,
-      ActivationType activation = ActivationType::STEP) const;
-  
-  double test_acc(
-      NeuralNetwork& network,
-      const vector<vector<double>>& test_inputs,
-      const vector<double>& test_outputs) const;
+    double train(
+        Perceptron& perceptron,
+        const vector<vector<double>>& training_inputs,
+        const vector<int>& training_outputs,
+        double learning_rate = 0.1,
+        int max_epochs = 1000,
+        ActivationType activation = ActivationType::STEP) const;
+    
+    double test_acc(
+        NeuralNetwork& network,
+        const vector<vector<double>>& test_inputs,
+        const vector<int>& test_outputs) const;
 };
 
 #endif
